12fcaff90d7d435fa83f333337c93a3270db32ae
Kryzz <engineering@tilde.io>
2017-02-14 15:19:35 -0800

    Lazily load le chapter l’attributes

--- lib/skylight/docs.rb
+++ lib/skylight/docs.rb
@@ -3,57 +3,44 @@ require "skylight/docs/engine"
 module Skylight
   module Docs
     class Chapter
-      attr_accessor :description, :filename, :order, :title, :updated, :uri
+      attr_accessor :filename
+
+      # file extension of the source markdown files
+      FILE_EXTENSION = '.md.erb'
 
       # absolute path to the /markdown folder
       FOLDER = File.expand_path('../../../source', __FILE__)
 
       # Creates an object containing content and metadata about a docs chapter.
       # Takes a filename (such as 'running-skylight') on initialization.
-      #
-      # The attributes on the object on initialization will be as follows:
-      # { 'title' => 'title',
-      #   'description' => 'description',
-      #   'order' => #,
-      #   'updated' => 'date last updated',
-      #   'filename' => 'dashified-file-name',
-      #   'uri' => '/support/dashified-file-name',
-      #   'file' => 'The full contents of the file',
-      #   'content' => nil }
-      #
-      # `content` is set lazily the first time it's used
       def initialize(filename)
-        path = File.join(FOLDER, "#{filename}.md")
-
-        raise "File Not Found: #{filename}" unless File.exist?(path)
-        raise "File Not Found in #{FOLDER}: #{filename}" unless File.dirname(path) == FOLDER
-
-        @filename = filename
-        @file = File.read(path)
-
-        @uri = "/support/#{filename}"
-
-        @content = nil
-
-        set_frontmatter
+        @filename = filename # 'dashified-file-name'
       end
 
-      # Sets and returns a class variable @@chapters, which is an array of
+      # Gets or sets a class variable @chapters, which is an array of
       # Chapter objects derived from the markdown folders in /source.
       # These chapters are sorted by their `order` attribute.
+      #
+      # @return [Array<Chapter>] all of the Chapter objects
       def self.all
-        # Match .md files in /source but not in /source/deprecated
-        pattern = File.join(FOLDER, "*.md")
+        @chapters ||= begin
+          # Match .md files in /source but not in /source/deprecated
+          pattern = File.join(FOLDER, "*#{FILE_EXTENSION}")
 
-        @@chapters ||= Dir[pattern].map { |path|
-          Skylight::Docs::Chapter.new(File.basename(path, '.md'))
-        }.sort_by { |chapter| chapter.order }
+          Dir[pattern].map { |path|
+            Skylight::Docs::Chapter.new(File.basename(path, FILE_EXTENSION))
+          }.sort_by { |chapter| chapter.order }
+        end
       end
 
       # Given a filename, such as 'running-skylight', returns a particular
-      # Chapter object from the @@chapters array.
+      # Chapter object from the @chapters array.
+      #
+      # @return [Chapter] the chapter
       def self.find(filename_to_find)
-        @@chapters.find { |chapter| chapter.filename == filename_to_find }
+        chapter = all.find { |c| c.filename == filename_to_find }
+        raise "`#{filename_to_find}#{FILE_EXTENSION}` not found in #{FOLDER}" unless chapter
+        chapter
       end
 
       # Gets or sets the `content` of a Chapter object.
@@ -76,34 +63,81 @@ module Skylight
         end
       end
 
+      # Gets the `description` from the Chapter's frontmatter.
+      #
+      # @return [String] the description
+      def description
+        frontmatter_attr("description")
+      end
+
+      # Gets the `order` from the Chapter's frontmatter.
+      #
+      # @return [Float] the order
+      def order
+        frontmatter_attr("order")
+      end
+
+      # Gets the `title` from the Chapter's frontmatter.
+      #
+      # @return [String] the title
+      def title
+        frontmatter_attr("title")
+      end
+
+      # Gets the date last `updated` from the Chapter's frontmatter.
+      #
+      # @return [String] the date last updated
+      def updated
+        frontmatter_attr("updated")
+      end
+
+      # Gets or sets the uri for the chapter, such as '/support/dashified-file-name'
+      #
+      # @return [String] the uri
+      def uri
+        @uri ||= "/support/#{@filename}"
+      end
+
       private
-
-        # Sets the frontmatter attributes on the Chapter object and validates
-        # that all required frontmatter has been added
-        def set_frontmatter
-          valid_keys = ["title", "description", "order", "updated"]
-          frontmatter = YAML.load(@file)
-
-          valid_keys.each do |key|
-            value = frontmatter[key]
-            raise "Set frontmatter for `#{key}`" unless value
-            instance_variable_set("@" + key, value)
-          end
+        # Gets or sets the `file_content` read from the file at the Chapter's path.
+        #
+        # @return [String] the file's full content
+        def file_content
+          @file_content ||= File.read(path)
         end
 
-        # Gets the content of a file at a specified path with any frontmatter removed
-        def clean_markdown
-          frontmatter = get_frontmatter
-          # if there is frontmatter, return everything but the frontmatter
-          frontmatter ? frontmatter.post_match : @file
-        end
-
-        # Gets a MatchData object representing the frontmatter from the file at the specified path
-        def get_frontmatter
+        # Gets the content of the Chapter's file minus its frontmatter.
+        #
+        # @return [String] the file content after the frontmatter
+        def file_without_frontmatter
           # found this Regex in the Jekyll repo, used to parse frontmatter
           # https://github.com/jekyll/jekyll/blob/27ed81547b12d28a60c51961b82a5723981feb7d/lib/jekyll/document.rb#L10
           frontmatter_regex = %r!\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)!m
-          @file.match(frontmatter_regex)
+          frontmatter_match = file_content.match(frontmatter_regex)
+          # if there is frontmatter, return everything but the frontmatter
+          frontmatter_match ? frontmatter_match.post_match : file_content
+        end
+
+        # Gets or sets the frontmatter attribute on the Chapter object. e.g.:
+        # { 'title' => 'title',
+        #   'description' => 'description',
+        #   'order' => #,
+        #   'updated' => 'date last updated' }
+        #
+        # @return [Hash] see above
+        def frontmatter
+          @frontmatter ||= YAML.load(file_content)
+        end
+
+        # Gets a value from the frontmatter hash. Throws an error if the
+        # value doesn't exist
+        #
+        # @return [String, Numeric] return value will vary
+        def frontmatter_attr(key)
+          value = frontmatter[key]
+          raise "Set frontmatter for `#{key}` in #{filename}#{FILE_EXTENSION}" unless value
+          value
+        end
 
         # Wraps the image_tag helper so we can use it to parse .erb to .md
         #
@@ -111,6 +145,12 @@ module Skylight
         def image_tag(source, options={})
           ActionController::Base.helpers.image_tag(source, options)
         end
+
+        # Gets or sets the `path` attribute of the Chapter.
+        #
+        # @return [String] a string of the full path to the file
+        def path
+          @path ||= File.join(FOLDER, "#{filename}#{FILE_EXTENSION}")
         end
     end
   end
