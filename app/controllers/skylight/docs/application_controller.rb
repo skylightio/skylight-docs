ad9f5d39dc305af2a0aacc295df2d32cdfea7bf6
Kryzz <engineering@tilde.io>
2017-02-17 15:54:53 -0800

    Remove unnecessary default Rails stuff

--- app/controllers/skylight/docs/application_controller.rb
+++ app/controllers/skylight/docs/application_controller.rb
@@ -1,7 +1,6 @@
 module Skylight
   module Docs
     class ApplicationController < ActionController::Base
-      protect_from_forgery with: :exception
     end
   end
 end
--- app/models/skylight/docs/application_record.rb
+++
@@ -1,7 +0,0 @@
-module Skylight
-  module Docs
-    class ApplicationRecord < ActiveRecord::Base
-      self.abstract_class = true
-    end
-  end
-end
