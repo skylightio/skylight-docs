bb8993f2097cf21878b47b5174e88bcb37c26a00
Peter Wagenet <peter.wagenet@gmail.com>
2017-03-07 11:37:31 -0800

    Correct revision handling

--- lib/skylight/docs/version.rb
+++ lib/skylight/docs/version.rb
@@ -1,6 +1,13 @@
 module Skylight
   module Docs
     VERSION = '0.1.0'
-    REVISION = File.basename(File.expand_path(__dir__))
+
+    root_dir = File.basename(File.expand_path("../../../../", __FILE__))
+    if root_dir =~ /-([0-9a-f]{6,})$/
+      REVISION = $1
+    else
+      warn "Skylight::Docs REVISION cannot be determined, using VERSION instead."
+      REVISION = VERSION
+    end
   end
 end
