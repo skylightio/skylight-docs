a686081970278e29c6793faa69e9cdce8f7b845c
zvkemp <zvkemp@gmail.com>
2019-09-16 14:18:31 -0700

    add markdown template handler

--- dummy_app/app/controllers/application_controller.rb
+++ dummy_app/app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action do
+    # FIXME: remove this
+    load Skylight::Docs::Engine.root.join("lib/skylight/docs/chapter.rb")
+    true
+  end
+
+  around_action :capture_errs
+
+  def capture_errs
+    yield
+  rescue => e
+    # binding.pry
+    raise
+  end
 end
