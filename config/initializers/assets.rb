027b048b61a36f7a0cb36a15a7e19bc41eda5dca
zvkemp <zvkemp@gmail.com>
2022-05-04 14:09:40 -0700

    fix tests for Rails 7

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += %w( skylight/docs/* )
+if Rails.application.config.respond_to?(:assets)
+  Rails.application.config.assets.precompile += %w( skylight/docs/* )
+end
--- dummy_app/config/initializers/docs_views.rb
+++ dummy_app/config/initializers/docs_views.rb
@@ -1,5 +1,7 @@
-ApplicationController.instance_exec do
-  before_action do
-    prepend_view_path Skylight::Docs::Engine.root.join('spec/test_source')
+Rails.application.reloader.to_prepare do
+  ApplicationController.instance_exec do
+    before_action do
+      prepend_view_path Skylight::Docs::Engine.root.join('spec/test_source')
+    end
   end
 end
