230bc87abb4d0e7627580e71e7584c9dc8408478
Godfrey Chan <godfreykfc@gmail.com>
2025-11-07 16:05:19 -0800

    Fix assets compilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 if Rails.application.config.respond_to?(:assets)
-  Rails.application.config.assets.precompile += %w( skylight/docs/* )
+  Rails.application.config.assets.precompile += %w( skylight_docs_manifest.js )
 end
